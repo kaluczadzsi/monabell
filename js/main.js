71fc33d2343b5e72ba78359f884261f9f5627656
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-16 17:56:47 +0200

    Js and animations added to navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Elements
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav__links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
