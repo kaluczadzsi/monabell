9216bdbe09f8086cf01321c384313f5489114570
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-23 18:17:39 +0200

    Refactoring some text

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
 const navListItem = document.querySelectorAll('.nav-list-item');
 const sectionNav = document.querySelector('.section-nav');
+const sectionHero = document.querySelector('.section-hero');
 
 hamburger.addEventListener('click', (e) => {
   slideMobileNav();
+  sectionNav.classList.toggle('bg-white');
 });
 
 navListItem.forEach((li) =>
@@ -16,5 +18,22 @@ navListItem.forEach((li) =>
 function slideMobileNav() {
   hamburger.classList.toggle('active');
   navList.classList.toggle('active');
-  sectionNav.classList.toggle('bg-white');
 }
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      sectionNav.classList.add('sticky');
+      sectionHero.classList.add('section-mt-10');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+
+obs.observe(sectionHero);
